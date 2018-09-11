257f3ecd9b59dee7d16428eb1e1149786924cd10
Gilles <alice.gilles.sio@gmail.com>
2018-09-11 16:42:11 +0200

    Classe Heure [youhou!]

--- Heure.java
+++ Heure.java
@@ -1,2 +1,12 @@
-class Heure { 
+public class Heure { 
+	
+	private int heures ; 
+	private int minutes ; 
+	
+	public void heure ( int, int) {
+		this.heures = heures ; 
+		this.minutes = minutes ; 
+	}
+	
+	
 }
